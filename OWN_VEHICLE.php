6254f938bed26f5f2d9a86a48022237098556251
kvermun <kartikvermun@gmail.com>
2014-02-27 01:25:10 -0800

    OwnVehicle

---
+++ OWN_VEHICLE.php
@@ -0,0 +1,111 @@
+<html>
+<head>
+<title>
+  OWN_VEHICLE
+</title>
+</head>
+<body>  
+
+<form name="OWN_VEHICLE" method="post" action="html_form_send.php">
+<table width="450px">
+<tr>
+ <td valign="top">
+  <label for="vehicle_Id">VEHICLE ID</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="vehicle_Id" maxlength="50" size="30">
+ </td>
+</tr>
+ 
+<tr>
+ <td valign="top"">
+  <label for="type">TYPE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="type" maxlength="50" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="make">MAKE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="make" maxlength="80" size="30">
+ </td>
+ 
+</tr>
+<tr>
+ <td valign="top">
+  <label for="model">MODEL</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="model" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="year_of_purchase">YEAR OF PURCHASE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="year_of_purchase" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="driver_type">DRIVER TYPE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="driver_type" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="sitting_capacity">SITTING CAPACITY</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="sitting_capacity" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="driver_name">DRIVER NAME</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="driver_name" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="licence">LICENCE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="licence" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="licence_date">LICENCE DATE</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="date" maxlength="10" size="10">
+ </td>
+  <td valign="top">
+  <input  type="text" name="month" maxlength="10" size="10">
+ </td>
+  <td valign="top">
+  <input  type="text" name="year" maxlength="10" size="10">
+ </td>
+</tr>
+
+</table>
+</form>
+
+</body>
+</html>
